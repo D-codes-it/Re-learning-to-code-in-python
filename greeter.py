a6dfd67a71b411cb53b66970b9dfc56c23f78d20
_Dina_ <bernardinasenadza880@gmail.com>
2026-01-23 16:54:58 +0000

    feat: code for greeter program

--- greeter.py
+++ greeter.py
@@ -1,1 +1,9 @@
-# creating a simple program that asks for the user's name
+# creating a simple program that asks for the user's name and birth year, then tells them their current age!
+user_name = input("Enter your name: ")
+birth_year = input("What year were you born? ")
+birth_year_num = int(birth_year)
+
+current_year = 2026
+user_age = current_year - birth_year_num
+
+print ("Hello " + user_name + "! " + "You are " + user_age + "years old." )
