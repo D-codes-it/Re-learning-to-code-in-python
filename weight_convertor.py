03e36fd6ba8cba7599f64b6b57ca160c23143724
_Dina_ <bsdina19@gmail.com>
2026-02-14 15:00:51 +0000

    feat: fixed str-float concatenation issue

--- weight_convertor.py
+++ weight_convertor.py
@@ -1,13 +1,13 @@
 weight = int(input("Weight: "))
 measure = print("Type 'K' if Kg or 'L'if Lbs")
-unit = input("(K)g or (L)bs: ")
+unit = input("Kg or Lbs: ")
 
 if unit == "L":
     weight_in_kg = weight / 2.2
-    print("Weight in Kg: " + (weight_in_kg))
+    print("Weight in Kg: " + str(weight_in_kg))
 elif unit == "K":
     weight_in_lbs = weight * 2.2
-    print("Weight in Lbs: " + (weight_in_lbs))
+    print("Weight in Lbs: " + str(weight_in_lbs))
 else:
     print("Cannot convert weight")
           
