24cd2298005db35ff3c055bbe5943d351aef1490
_Dina_ <bsdina19@gmail.com>
2026-02-14 15:46:50 +0000

    feat: fixed case sensitivity issue

--- weight_convertor.py
+++ weight_convertor.py
@@ -1,11 +1,11 @@
 weight = float(input("Weight: "))
-measure = print("Type 'K' if Kg or 'L'if Lbs")
+measure = print("Type 'K/k' if Kg or 'L/l'if Lbs")
 unit = input("Kg or Lbs: ")
 
-if unit == "L":
+if unit.upper() == "L":
     weight_in_kg = weight // 2.2
     print("Weight in Kg: " + str(weight_in_kg))
-elif unit == "K":
+elif unit.upper() == "K":
     weight_in_lbs = weight * 2.2
     print("Weight in Lbs: " + str(weight_in_lbs))
 else:
