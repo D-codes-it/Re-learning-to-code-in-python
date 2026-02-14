3c49b5c2a3509a2c2393c524ad599e1ef7d7cd68
_Dina_ <bsdina19@gmail.com>
2026-02-14 14:58:24 +0000

    feat: code for weight_convertor program

---
+++ weight_convertor.py
@@ -0,0 +1,13 @@
+weight = int(input("Weight: "))
+measure = print("Type 'K' if Kg or 'L'if Lbs")
+unit = input("(K)g or (L)bs: ")
+
+if unit == "L":
+    weight_in_kg = weight / 2.2
+    print("Weight in Kg: " + (weight_in_kg))
+elif unit == "K":
+    weight_in_lbs = weight * 2.2
+    print("Weight in Lbs: " + (weight_in_lbs))
+else:
+    print("Cannot convert weight")
+          
