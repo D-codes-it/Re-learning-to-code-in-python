e2c87efd23aedfa16308db0414605e109c518e2a
_Dina_ <bsdina19@gmail.com>
2026-02-14 15:09:19 +0000

    feat: perform floor division for conversion and accept float weight values

--- weight_convertor.py
+++ weight_convertor.py
@@ -1,9 +1,9 @@
-weight = int(input("Weight: "))
+weight = float(input("Weight: "))
 measure = print("Type 'K' if Kg or 'L'if Lbs")
 unit = input("Kg or Lbs: ")
 
 if unit == "L":
-    weight_in_kg = weight / 2.2
+    weight_in_kg = weight // 2.2
     print("Weight in Kg: " + str(weight_in_kg))
 elif unit == "K":
     weight_in_lbs = weight * 2.2
